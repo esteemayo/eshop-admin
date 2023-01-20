56130cfafa2394793aee067f1f9063049c8dc7e3
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-20 08:45:45 +0100

    fix: comment out

--- src/redux/apiCalls/authApiCalls.js
+++ src/redux/apiCalls/authApiCalls.js
@@ -1,30 +1,30 @@
-import * as actions from '../user/userSlice';
-import { setToStorage, tokenKey } from 'utils';
-import * as authService from 'services/authService';
+// import * as actions from '../user/userSlice';
+// import { setToStorage, tokenKey } from 'utils';
+// import * as authService from 'services/authService';
 
-export const loginUser = async (userData, dispatch) => {
-  dispatch(actions.loginStart());
+// export const loginUser = async (userData, dispatch) => {
+//   dispatch(actions.loginStart());
 
-  try {
-    const { data } = await authService.login(userData);
-    setToStorage(tokenKey, data.details);
-    data.role === 'admin' && dispatch(actions.loginSuccess(data.details));
-  } catch (err) {
-    dispatch(actions.loginFailure());
-    console.log(err.response);
-  }
-};
+//   try {
+//     const { data } = await authService.login(userData);
+//     setToStorage(tokenKey, data.details);
+//     data.role === 'admin' && dispatch(actions.loginSuccess(data.details));
+//   } catch (err) {
+//     dispatch(actions.loginFailure());
+//     console.log(err.response);
+//   }
+// };
 
-export const registerUser = async (userData, dispatch) => {
-  dispatch(actions.registerUserStart());
+// export const registerUser = async (userData, dispatch) => {
+//   dispatch(actions.registerUserStart());
 
-  try {
-    const {
-      data: { details },
-    } = await authService.register(userData);
-    dispatch(actions.registerUserSuccess(details));
-  } catch (err) {
-    dispatch(actions.registerUserFailure());
-    console.log(err.response);
-  }
-};
+//   try {
+//     const {
+//       data: { details },
+//     } = await authService.register(userData);
+//     dispatch(actions.registerUserSuccess(details));
+//   } catch (err) {
+//     dispatch(actions.registerUserFailure());
+//     console.log(err.response);
+//   }
+// };
