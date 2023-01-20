58cceafe357c9c3a3a2a79598162430937cdf23d
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-20 14:45:41 +0100

    fix: modify

--- src/services/productService.js
+++ src/services/productService.js
@@ -2,30 +2,21 @@ import http from './httpService';
 
 const apiEndpoint = '/products';
 
-const productUrl = (productID) => {
-  return `${apiEndpoint}/${productID}`;
-};
+const productUrl = (productID) => `${apiEndpoint}/${productID}`;
 
-export function getProducts() {
-  return http.get(apiEndpoint);
-}
+export const getProducts = () => http.get(apiEndpoint);
 
-export function getProductById(productID) {
-  return http.get(productUrl(productID));
-}
+export const getProductById = (productID) =>
+  http.get(productUrl(productID));
 
-export function getProductBySlug(slug) {
-  return http.get(`${apiEndpoint}/details/${slug}`);
-}
 
-export function createProduct(data) {
-  return http.post(apiEndpoint, data);
-}
+export const getProductBySlug = (slug) =>
+  http.get(`${apiEndpoint}/details/${slug}`);
 
-export function updateProduct(productID, data) {
-  return http.patch(productUrl(productID), data);
-}
+export const createProduct = (data) => http.post(apiEndpoint, data);
 
-export function deleteProduct(productID) {
-  return http.delete(productUrl(productID));
-}
+export const updateProduct = (productID, data) =>
+  http.patch(productUrl(productID), data);
+
+export const deleteProduct = (productID) =>
+  http.delete(productUrl(productID));
