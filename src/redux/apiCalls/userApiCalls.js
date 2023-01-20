5c5d27f1844ac08151400aa14264f45816312a37
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-20 08:45:55 +0100

    fix: comment out

--- src/redux/apiCalls/userApiCalls.js
+++ src/redux/apiCalls/userApiCalls.js
@@ -1,28 +1,28 @@
-import * as actions from '../user/userSlice';
-import * as userService from 'services/userService';
+// import * as actions from '../user/userSlice';
+// import * as userService from 'services/userService';
 
-export const fetchUsers = async (dispatch) => {
-  dispatch(actions.getUserStart());
+// export const fetchUsers = async (dispatch) => {
+//   dispatch(actions.getUserStart());
 
-  try {
-    const {
-      data: { users },
-    } = await userService.getAllUsers();
-    dispatch(actions.getUserSuccess(users));
-  } catch (err) {
-    dispatch(actions.getUserFailure());
-    console.log(err.response);
-  }
-};
+//   try {
+//     const {
+//       data: { users },
+//     } = await userService.getAllUsers();
+//     dispatch(actions.getUserSuccess(users));
+//   } catch (err) {
+//     dispatch(actions.getUserFailure());
+//     console.log(err.response);
+//   }
+// };
 
-export const removeUser = async (userId, dispatch) => {
-  dispatch(actions.deleteUserStart());
+// export const removeUser = async (userId, dispatch) => {
+//   dispatch(actions.deleteUserStart());
 
-  try {
-    await userService.deleteUser(userId);
-    dispatch(actions.deleteUserSuccess(userId));
-  } catch (err) {
-    dispatch(actions.deleteUserFailure());
-    console.log(err.response);
-  }
-};
+//   try {
+//     await userService.deleteUser(userId);
+//     dispatch(actions.deleteUserSuccess(userId));
+//   } catch (err) {
+//     dispatch(actions.deleteUserFailure());
+//     console.log(err.response);
+//   }
+// };
