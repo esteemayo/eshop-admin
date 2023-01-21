09cd97239631d785251981522bd24d6ea6023c44
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-21 15:40:01 +0100

    feat: add dark/light mode color theme

---
+++ src/utils/Theme.js
@@ -0,0 +1,13 @@
+export const darkTheme = {
+  bg: '#181818',
+  text: 'rgb(156, 156, 156)',
+  textSoft: '#aaaaaa',
+  soft: '#373737',
+};
+
+export const lightTheme = {
+  bg: 'var(--color-white)',
+  text: '#333333',
+  textSoft: '#606060',
+  soft: '#f5f5f5',
+};
