5d479681d9277da04214317afccc9796d4ccf599
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-21 09:54:41 +0100

    feat: add dark mode reducer

--- src/app/store.js
+++ src/app/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 
 import userReducer from 'redux/user/userSlice';
 import productReducer from 'redux/products/productSlice';
+import darkModeReducer from 'redux/darkMode/darkModeSlice';
 
 const store = configureStore({
   reducer: {
     user: userReducer,
     product: productReducer,
+    darkMode: darkModeReducer,
   },
 });
 
