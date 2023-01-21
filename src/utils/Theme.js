bc63562cbbf6536d90bf8b023b17a8b4fdcf894a
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-21 15:46:41 +0100

    fix: modify

--- src/utils/Theme.js
+++ src/utils/Theme.js
@@ -1,5 +1,6 @@
 export const darkTheme = {
   bg: '#181818',
+  bgLight: '#181818',
   text: 'rgb(156, 156, 156)',
   textSoft: '#aaaaaa',
   soft: '#373737',
@@ -7,6 +8,7 @@ export const darkTheme = {
 
 export const lightTheme = {
   bg: 'var(--color-white)',
+  bgLight: 'rgb(251, 251, 255)',
   text: '#333333',
   textSoft: '#606060',
   soft: '#f5f5f5',
