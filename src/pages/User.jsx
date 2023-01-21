e633aa2613a0d79de91b4175b6b376142f826add
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-21 07:57:48 +0100

    fix: import useEffect react hook

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { useSelector } from 'react-redux';
 import { Link, useLocation } from 'react-router-dom';
