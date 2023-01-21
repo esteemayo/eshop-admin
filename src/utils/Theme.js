8e64b4fe8cc72cf3835a776e694e61b4175b562c
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-21 15:56:26 +0100

    fix: add new color

--- src/utils/Theme.js
+++ src/utils/Theme.js
@@ -1,9 +1,10 @@
 export const darkTheme = {
   bg: '#181818',
-  bgLight: '#181818',
+  bgLight: '#202020',
   text: 'rgb(156, 156, 156)',
   textSoft: '#aaaaaa',
   soft: '#373737',
+  logo: 'var(--color-white)',
 };
 
 export const lightTheme = {
@@ -12,4 +13,5 @@ export const lightTheme = {
   text: '#333333',
   textSoft: '#606060',
   soft: '#f5f5f5',
+  logo: '#00008b',
 };
