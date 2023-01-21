6b45dace617d3c82ada7b725c4788fe8d117b037
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-22 00:17:51 +0100

    fix: handle dark/light mode

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -440,7 +440,7 @@ const Input = styled.input`
   display: block;
   width: 50rem;
   padding: 1rem 2rem;
-  background-color: transparent;
+  background-color: ${({ theme }) => theme.bgInput};
   color: #999;
   border-bottom: 3px solid #bbb;
   caret-color: #00008b;
@@ -469,7 +469,7 @@ const Select = styled.select`
   width: 100%;
   display: block;
   padding: 1rem 2rem;
-  background-color: transparent;
+  background-color: ${({ theme }) => theme.bgInput};
   color: #bbb;
   user-select: none;
   border-bottom: 3px solid #bbb;
