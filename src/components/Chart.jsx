c1fd5f6c9846f52fc327861af105706540a1a159
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-21 19:31:07 +0100

    fix: handle dark/light mode

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -1,3 +1,4 @@
+import { useSelector } from 'react-redux';
 import styled from 'styled-components';
 import {
   LineChart,
@@ -10,13 +11,15 @@ import {
 } from 'recharts';
 
 const Chart = ({ title, data, dataKey, grid }) => {
+  const { darkMode } = useSelector((state) => state.darkMode);
+
   return (
     <Container>
       <Title>{title}</Title>
       <ResponsiveContainer width='100%' aspect={4 / 1}>
         <LineChart data={data}>
-          <XAxis dataKey='name' stroke='#5550bd' />
-          <Line type='monotone' dataKey={dataKey} stroke='#5550bd' />
+          <XAxis dataKey='name' stroke={darkMode ? '#aaaaaa' : '#5550bd'} />
+          <Line type='monotone' dataKey={dataKey} stroke={darkMode ? '#aaaaaa' : '#5550bd'} />
           <Tooltip />
           {grid && <CartesianGrid stroke='#e0dfdf' strokeDasharray='5 5' />}
           <Legend />
