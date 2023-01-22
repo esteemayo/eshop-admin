98617daf9733e629009a057f1efe2b71655ecde4
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-22 09:31:54 +0100

    fix: rename file

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,8 @@
-import Error from './Error';
 import Home from './Home';
 import Login from './Login';
 import NewProduct from './NewProduct';
 import NewUser from './NewUser';
+import NotFound from './NotFoundd';
 import Product from './Product';
 import ProductList from './ProductList';
 import SharedLayout from './SharedLayout';
@@ -10,11 +10,11 @@ import User from './User';
 import UserList from './UserList';
 
 export {
-  Error,
   Home,
   Login,
   NewProduct,
   NewUser,
+  NotFound,
   Product,
   ProductList,
   SharedLayout,
