4a88e2996963ef071ce2bd4b7b1b6e78fc73d7d7
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-23 06:46:10 +0100

    fix: import redux-persist

--- src/app/store.js
+++ src/app/store.js
@@ -1,4 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
 import userReducer from 'redux/user/userSlice';
 import productReducer from 'redux/products/productSlice';
