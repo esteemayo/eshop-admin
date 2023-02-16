57575cc83a548e52fe3cc605c1f489dead06feba
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-17 00:26:36 +0100

    fix: refactor

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -40,8 +40,6 @@ const Product = () => {
   const [inputs, setInputs] = useState(initialState);
   const [productStats, setProductStats] = useState([]);
 
-  const productId = id;
-
   const handleChange = ({ target: input }) => {
     const { name, value } = input;
     setInputs((prev) => ({ ...prev, [name]: value }));
