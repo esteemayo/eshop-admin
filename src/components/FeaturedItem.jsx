86bb81dca79deb234a6d0707e82bd01bfb06ad6a
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-17 00:47:42 +0100

    feat: import ArrowDownward/ArrowUpward

--- src/components/FeaturedItem.jsx
+++ src/components/FeaturedItem.jsx
@@ -1,9 +1,66 @@
 import styled from 'styled-components';
+import { ArrowDownward, ArrowUpward } from '@material-ui/icons';
 
 const FeaturedItem = () => {
   return (
-    <div>FeaturedItem</div>
+    <Container>
+      <Title>Sales</Title>
+      <FeaturedMoneyContainer>
+        <FeaturedMoney>$4,415</FeaturedMoney>
+        <FeaturedMoneyRate>
+          %-1.4{' '}
+          <ArrowDownward
+            style={{
+              fontSize: '1.4rem',
+              marginLeft: '0.5rem',
+              color: 'red',
+            }}
+          />{' '}
+        </FeaturedMoneyRate>
+      </FeaturedMoneyContainer>
+      <FeaturedSub>Compared to last month</FeaturedSub>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  flex: 1;
+  font-size: 1.6rem;
+  font-weight: 300;
+  margin: 0 2rem;
+  padding: 3rem;
+  border-radius: 1rem;
+  cursor: pointer;
+  -webkit-box-shadow: ${({ theme }) => theme.box};
+  -moz-box-shadow: ${({ theme }) => theme.box};
+  box-shadow: ${({ theme }) => theme.box};
+`;
+
+const Title = styled.span`
+  font-size: 2rem;
+  color: ${({ theme }) => theme.textSoft};
+`;
+
+const FeaturedMoneyContainer = styled.div`
+  margin: 1rem 0;
+  display: flex;
+  align-items: center;
+`;
+
+const FeaturedMoney = styled.span`
+  font-size: 3rem;
+  font-weight: 600;
+`;
+
+const FeaturedMoneyRate = styled.span`
+  display: flex;
+  align-items: center;
+  margin-left: 2rem;
+`;
+
+const FeaturedSub = styled.span`
+  font-size: 1.5rem;
+  color: ${({ theme }) => theme.textSoft};
+`;
 
 export default FeaturedItem;
