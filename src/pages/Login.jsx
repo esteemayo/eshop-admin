94d255f1d3d7e0eddbb2065d132bccd4f961ca03
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-16 15:04:57 +0100

    fix: modify

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 import { toast } from 'react-toastify';
 import { useEffect, useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import Visibility from '@mui/icons-material/Visibility';
 import { useLocation, useNavigate } from 'react-router-dom';
 import VisibilityOff from '@mui/icons-material/VisibilityOff';
 import FaceOutlinedIcon from '@mui/icons-material/FaceOutlined';
