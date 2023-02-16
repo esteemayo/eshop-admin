cb91b9a3598ac710e7ada8fe4c4632a2e6c0afd3
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-17 00:32:09 +0100

    fix: handle clean up function

--- src/services/orderService.js
+++ src/services/orderService.js
@@ -5,7 +5,8 @@ const apiEndpoint = '/orders';
 export const getOrders = (token) =>
   http.get(apiEndpoint, { cancelToken: token });
 
-export const getIncome = () => http.get(`${apiEndpoint}/income`);
+export const getIncome = (token) =>
+  http.get(`${apiEndpoint}/income`, { cancelToken: token });
 
 export const getIncomeStats = (productID, token) =>
   http.get(`${apiEndpoint}/income?pid=${productID}`, { cancelToken: token });
