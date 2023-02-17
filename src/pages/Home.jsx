5920b2ae767f9463b05a1ac1e7654dbb75fdf1a4
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-18 00:09:20 +0100

    fix: modify

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
-import styled from 'styled-components';
 import { useSelector } from 'react-redux';
+import styled from 'styled-components';
 import { useCallback, useEffect, useMemo, useState } from 'react';
 
 import { phone } from 'responsive';
