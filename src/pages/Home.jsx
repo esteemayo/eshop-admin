7c2863a5075a4d0f52b2e8f59535aa32cfdcce16
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-18 00:52:07 +0100

    fix: refactor

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
-import axios from 'axios';
 import styled from 'styled-components';
+import axios from 'axios';
 import { useEffect, useMemo, useState } from 'react';
 
 import Chart from 'components/Chart';
