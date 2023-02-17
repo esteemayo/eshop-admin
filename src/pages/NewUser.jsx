47f3ff874fba1be337255a0a24544f38bd7b8228
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-18 00:28:51 +0100

    fix: modify

--- src/pages/NewUser.jsx
+++ src/pages/NewUser.jsx
@@ -10,7 +10,8 @@ import {
   getDownloadURL,
 } from 'firebase/storage';
 
-import { phone } from 'responsive';
+import app from '../firebase';
+import app from '../firebase';
 import { userInputs } from 'formData';
 import { registerUser, reset } from 'redux/user/userSlice';
 
