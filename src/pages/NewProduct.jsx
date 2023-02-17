0348686edd8ebf88a545ed7ce77bd3216f338708
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-18 00:25:39 +0100

    fix: modify

--- src/pages/NewProduct.jsx
+++ src/pages/NewProduct.jsx
@@ -1,5 +1,4 @@
 import styled from 'styled-components';
-import { toast } from 'react-toastify';
 import { useRef, useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import {
