d2a5a1cf5ed1afbf2a5479153341e47a645516eb
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-18 01:18:01 +0100

    feat: import material icons

--- src/pages/NewUser.jsx
+++ src/pages/NewUser.jsx
@@ -3,6 +3,12 @@ import { useState, useEffect } from 'react';
 import { toast } from 'react-toastify';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import FaceOutlinedIcon from '@mui/icons-material/FaceOutlined';
+import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
+import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
+import FileUploadOutlinedIcon from '@mui/icons-material/FileUploadOutlined';
 import {
   getStorage,
   ref,
