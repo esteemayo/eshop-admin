3774f499e3d481ecc0acdde7f2cb783272cd4525
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-18 01:29:09 +0100

    fix: modify

--- src/pages/NewUser.jsx
+++ src/pages/NewUser.jsx
@@ -6,7 +6,7 @@ import { useNavigate } from 'react-router-dom';
 import VisibilityOff from '@mui/icons-material/VisibilityOff';
 import Visibility from '@mui/icons-material/Visibility';
 import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
-import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
+import FaceOutlinedIcon from '@mui/icons-material/FaceOutlined';
 import FileUploadOutlinedIcon from '@mui/icons-material/FileUploadOutlined';
 import {
   getStorage,
