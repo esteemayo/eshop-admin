ad8438793a9815153faff6e764072bf6ec5b5070
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-20 07:08:09 +0100

    fix: modify

--- src/components/FeaturedItem.jsx
+++ src/components/FeaturedItem.jsx
@@ -80,7 +80,7 @@ const FeaturedItem = ({ type }) => {
     case 'cost':
       data = {
         title: 'Cost',
-        money: '2,415',
+        money: '2415',
         rate: '2.4',
         icon: (
           <ArrowUpward
